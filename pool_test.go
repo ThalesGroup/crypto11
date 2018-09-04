21080267e7212b86dc24f896a5f3e4f9db84a65f
John.Koepi <john.koepi@gmail.com>
2018-09-04 19:22:19 +0200

    Regression and behavior test for handling login sessions with the resource pool

---
+++ pool_test.go
@@ -0,0 +1,81 @@
+// Copyright 2018 Thales e-Security, Inc
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+package crypto11
+
+import (
+	"crypto/elliptic"
+	"encoding/json"
+	"github.com/miekg/pkcs11"
+	"log"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestPoolTimeout(t *testing.T) {
+	cfg, err := getConfig("config")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if cfg.Pin == "" {
+		t.Fatal("invalid configuration. configuration must have PIN non empty.")
+	}
+
+	_, err = Configure(cfg)
+	if err != nil {
+		t.Fatal("failed to configure service:", err)
+	}
+
+	time.Sleep(idleTimeout + 2*time.Second)
+
+	_, err = GenerateECDSAKeyPair(elliptic.P256())
+	if err != nil {
+		if perr, ok := err.(pkcs11.Error); ok && perr == pkcs11.CKR_USER_NOT_LOGGED_IN {
+			t.Fatal("pool handle session incorrectly, login required but missing:", err)
+		} else {
+			t.Fatal("failed to generate a key, unexpected error:", err)
+		}
+	}
+
+	// TODO: check handling of handles from previous sessions with pkcs11: 0x82: CKR_OBJECT_HANDLE_INVALID
+	// TODO: make timeout configurable. timeouts configuration must stick to the pool instance.
+	// TODO: reduce idle timeout in test with the help of configuration.
+}
+
+func getConfig(configLocation string) (*PKCS11Config, error) {
+	file, err := os.Open(configLocation)
+	if err != nil {
+		log.Printf("Could not open config file: %s", configLocation)
+		return nil, err
+	}
+	defer file.Close()
+	configDecoder := json.NewDecoder(file)
+	config := &PKCS11Config{}
+	err = configDecoder.Decode(config)
+	if err != nil {
+		log.Printf("Could decode config file: %s", err.Error())
+		return nil, err
+	}
+	return config, nil
+}
+
